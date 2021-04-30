81918e952a0cca74cb83e5e063085afa07be2a0a
Fawwaz30 <fawwazaf301101@gmail.com>
2021-05-01 00:56:30 +0700

    penambhaan views Laporan Barang Keluar

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,9 @@ Route::group(['middleware' => ['auth']], function () {
         'laporan/barang',
         [ItemController::class, 'report']
     )->name('laporan.barang');
+
+    Route::get(
+        'laporan/keuangan',
+        [FinanceController::class, 'report']
+    )->name('laporan.keuangan');
 });
